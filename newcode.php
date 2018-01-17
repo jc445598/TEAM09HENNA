bc2ed0b9af769ff1f9dc8c3092ff354bb988a262
Harpal007 <harpalsingh.chahal@my.jcu.edu.au>
2018-01-17 16:05:27 +1000

    Add files via upload

---
+++ newcode.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Henna Art</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" ref="css/main.css">
+  <link rel="shortcut icon" href="../favicon.ico">
+  <link rel="stylesheet" type="text/css" href="css/default.css" />
+  <link rel="stylesheet" type="text/css" href="css/component.css" />
+  <script src="js/modernizr.custom.js"></script>
+  
+  <!-- below these three bootstrap CDN -->
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+  
+  <!-- Below this bootstrap link is for social icons-->
+  
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+</head>
+
+
+
+<body>
+
+
+<header class="container text-success">
+	
+		<nav class="container navbar navbar-default navbar-fixed-top ">
+  
+					<div class="navbar-header">
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false" aria-controls="navbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						  </button>
+						  <a class="navbar-brand text-success" href="#">WebSiteName</a>
+					</div>
+
+						<div class="collapse navbar-collapse" id="myNavbar">
+
+							<ul class="nav navbar-nav text-success">
+
+							<li class="active"><a href="#">Home</a></li>
+							<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">SERVICES <span class="caret"></span></a>
+							<ul class="dropdown-menu text-success">
+							<li><a href="#">BRIDAL</a></li>
+							<li><a href="#">FOOT</a></li>
+							<li><a href="#">BELLY</a></li>
+							<li><a href="#">KIDS</a></li>
+							<li><a href="#">MENS</a></li>
+
+							</ul>
+							</li>
+							<li><a href="#">SHOP</a></li>
+							<li ><a href="#">ABOUT HENNA</a></li>
+							<li ><a href="#">CONTACT US</a></li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-check"></span> Book Here</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Buy</a></li>
+							</ul>
+
+						</div>
+			</nav>
+
+					<div class="page-header" style="margin-bottom:25px;"></div>
+		<div class="row">
+
+				<div class="col-lg-3">
+				<h4>Art of Henna</h4>
+								
+				</div>
+				<div class="col-lg-3"></div>
+				<div class="col-lg-3"></div>
+
+					<div class="col-lg-3">
+						<h5>
+						<span class="glyphicon glyphicon-phone"></span>
+						<i>Call Us +0123456789</i>
+							<div class="pull-right">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-linkedin"></i></a>
+							<a href="#"><i class="fa fa-github"></i></a>
+							</div>
+						</h5>
+
+					</div>
+
+		</div>
+		
+
+			
+
+
+</header>
+
+
+<section class="container-fluid"> <!-- slider container fluid start-->
+<!-- silder -->
+
+		<div id="myCarousel" class="carousel slide" data-ride="carousel"><!-- carousel div start -->
+			<!-- Indicators -->
+
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="images/12.jpg" alt="Chania">
+						<div class="carousel-caption">
+							<h3>Hand Design</h3>
+							<p>It would differentiate you. </p>
+						</div>
+				</div>
+
+				<div class="item">
+					<img src="images/13.jpg" alt="Chicago">
+						<div class="carousel-caption">
+							<h3>Tid Design</h3>
+							<p>It would differentiate you. </p>
+						</div>
+				</div>
+
+				<div class="item">
+					<img src="images/14.jpg" alt="New York">
+						<div class="carousel-caption">
+							<h3>Foot Design</h3>
+							<p>It would differentiate you. </p>
+						</div>
+				</div>
+
+
+<!-- Left and right arrow  controls -->
+
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					<span class="sr-only">Next</span>
+				</a> 
+
+			</div> <!--wrapper div ends-->
+
+		</div><!-- end of carousel div-->
+<div class="page-header" style="margin-bottom:25px;"></div>
+</section> <!-- End of silder container fluid -->
+
+<!-- content -->
+
+<div id="content">
+				<!-- About us content in article tag-->
+				<article>
+					<div class="container-fluid bg-gray" style="padding-top:10px;">
+						<div class="row">
+							<div class="col-lg-1"></div>
+								<div class="col-lg-8" style="color:green;font:normal 36px 'Cookie', cursive;">
+									<h2><strong>About Henna Art</strong></h2>
+									<p>Henna traditions have grown wherever the plant grows natively, so it it a cross-cultural and multi-faith art form.
+										No single culture can claim ownership of henna as a body adornment. Henna body art is now widely recognized in many countries and
+										is well deserving of it's reputation of timeless beauty. Style and grace are epitomized by this ancient tradition.
+										Henna is a unique addition to a hen's night, special event, party, wedding or function.
+									</p> 
+					  
+									<button class="btn btn-default btn-lg">Read More...</button>
+								</div>
+							<div class="col-lg-2">
+								<a href="#demo" data-toggle="collapse">
+									<img src="images/henna.gif" class="img-circle henna" alt="Random Name"></a>
+							</div>
+							<div class="col-lg-1"></div>
+						</div>
+					</div>
+			<div class="page-header" style="margin-bottom:25px;"></div>
+				</article>
+				<!-- end of about us code-->
+		<!--booking content in section tag -->
+	<section class="container">
+
+			<header class="clearfix">
+				<span> 
+					<h1 style="color:#47a3da;">Booking Options</h1>
+						<span class="bp-icon bp-icon-about" data-content="You can choose any option given below.
+							If you are new or looking for design or any doubt in your mind about the artist work
+							you should go through portfolio option so that you could see multiple designs and select one of them for yourself.
+							Along with this , we also provide free consultation facility for bridal only.">
+						</span>
+				</span>
+			</header>
+				
+				
+
+			<div class="main">
+				<ul class="cbp-ig-grid">
+					<li>
+						<a href="#">
+							<span class="cbp-ig-icon"> <!--cbp-ig-icon-phone-->
+							<img src="images/contact1_1.png">
+							</span>
+							<h3 class="cbp-ig-title">Contact</h3>
+							<span class="cbp-ig-category">Booking </span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="cbp-ig-icon"><!-- cbp-ig-icon-spectacles"-->
+							<img src="images/portfolio1_2.png">
+							</span>
+							<h3 class="cbp-ig-title">Portfolio</h3>
+							<span class="cbp-ig-category">Booking</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="cbp-ig-icon"><!-- cbp-ig-icon-whippy"-->
+							<img src="images/consultation1_1.png">
+							</span>
+							<h3 class="cbp-ig-title">Consultation</h3>
+							<span class="cbp-ig-category">Booking</span>
+						</a>
+					</li>
+
+				</ul>
+			</div>
+	</section>
+			<div class="page-header" style="margin-bottom:25px;"></div>
+</div>
+<!-- footer -->
+
+<div id="footer" >
+		<div class="footer1 container text-success ">
+			<div class="row">
+			
+				<div class="col-lg-4" style="padding:10px;font: normal 26px 'Cookie', cursive;">
+				<span class=" border-top-0 border-primary">
+						<h2><strong>Henna<span>logo</span></strong></h2>
+						<div class="footer-links">
+						<p>
+							<a class="text-success" href="#">Home</a>
+							·
+							<a class="text-success" href="#">Service</a>
+							·
+							<a class="text-success" href="#">Shop</a>
+							·
+							<a class="text-success" href="#">About Us</a>
+							·
+							<a class="text-success" href="#">Contact</a>
+						</p>
+
+						<p> Henna  &copy; 2017</p>
+					
+						</div>
+				</div></span>
+				<div class="col-lg-4" style="padding:10px;font: normal 26px 'Cookie', cursive;">
+
+						<div>
+						
+							<i class="fa fa-map-marker"></i>
+							<p><span>Not Found Street</span> NoWhere, Moon</p>
+						</div>
+
+						<div>
+							<i class="fa fa-phone"></i>
+							<p>123456789</p>
+						</div>
+
+						<div >
+							<i class="fa fa-envelope "></i>
+							<p><a class="text-success" href="mailto:support@company.com">don'tknow@company.com</a></p>
+						</div>
+
+					
+				</div>
+				
+					<div class="col-lg-4" style="padding:10px;font: normal 26px 'Cookie', cursive;">
+						<!--div class="footer-right"-->
+
+							<p>
+								<strong>About the Henna</Strong><br>
+								 Henna body art is now widely recognized in many countries and is well deserving of it's reputation of timeless beauty. 
+							</p>
+
+							<div >
+
+								<a class="text-success" href="#"><i class="fa fa-facebook"></i></a>
+								<a class="text-success" href="#"><i class="fa fa-twitter"></i></a>
+								<a class="text-success" href="#"><i class="fa fa-linkedin"></i></a>
+								<a class="text-success" href="#"><i class="fa fa-github"></i></a>
+
+							</div>
+						
+					</div>
+			</div>
+		</div>
+</div>
+
+	
+</body>
+</html>
